26682f6f52d271b57030bfa7543312387b09d37d
Mohamed_Mahfouz <98268832+MohamedMahfouz2022@users.noreply.github.com>
2024-07-20 18:03:01 +0300

    Update Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,7 +5,7 @@ const photo = () => {
     return (
         <div className="w-full h-full relative">
             <div className="w-[250px] h-[200px] xl:w-[400px] xl:h-[350px]">
-                <Image src="https://storyset.com/illustration/coding/bro#default&hide=&hide=false"
+                <Image src="/Coding-bro.png"
                     className="object-contain"
                     priority
                     quality={100}
