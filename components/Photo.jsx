576c392e3c4ac8f12756fd66434539e3afdf4508
Mohamed_Mahfouz <wrzdwrzd@gmail.com>
2024-07-25 00:23:44 +0300

    1

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -5,12 +5,12 @@ const photo = () => {
     return (
         <div className="w-full h-full relative">
             <div className="w-[250px] h-[200px] xl:w-[400px] xl:h-[350px]">
-                <Image src="/assets/Coding-bro.png"
+                <Image src="/Coding-bro.png"
                     className="object-contain"
                     priority
                     quality={100}
                     fill
-                    alt=""
+                    alt="Coding-bro.png"
                 />
             </div>
         </div>
