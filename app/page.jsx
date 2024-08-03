f5edbe6f8859f87ad83bc0efb3fe3c71a9158def
Mohamed_Mahfouz <wrzdwrzd@gmail.com>
2024-08-03 19:25:50 +0300

    update

--- app/page.jsx
+++ app/page.jsx
@@ -12,25 +12,59 @@ const Home = () => {
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: .7, duration: 0.4, ease: "easeInOut" }
+        transition: { delay: .5, duration: 0.7, ease: "easeInOut" }
       }}>
       <div className="container mx-auto h-full ">
         <div className="flex flex-col-reverse xl:flex-row items-center justify-around xl:pt-20 xl:pb-20">
           <div className="flex flex-col gap-5 items-center xl:items-start ">
             {/* text content */}
-            <h2 className="">Front End Web Developer</h2>
-            <h1 className="text-3xl">Hello I'm </h1>
-            <h1 className="text-3xl text-accent">Mohammad Mahfouz</h1>
-            <p className="text-center xl:text-start max-w-[500px] mb-8 text-white/80 ">I excel at crafting digital experiences and I am proficient in various programming languages and technologies.</p>
+            <motion.h2
+              initial={{ translateX: -50, opacity: 0 }}
+              animate={{
+                opacity: 1,
+                translateX: 0,
+                transition: { delay: .8, duration: 0.7, ease: "easeInOut" }
+              }}>Front End Web Developer</motion.h2>
+            <motion.h1 className="text-3xl"
+              initial={{ translateX: -50, opacity: 0 }}
+              animate={{
+                opacity: 1,
+                translateX: 0,
+                transition: { delay: .9, duration: 0.7, ease: "easeInOut" }
+              }}>Hello I'm </motion.h1>
+            <motion.h1 className="text-3xl text-accent"
+              initial={{ translateX: -50, opacity: 0 }}
+              animate={{
+                opacity: 1,
+                translateX: 0,
+                transition: { delay: 1, duration: 0.7, ease: "easeInOut" }
+              }}>Mohammad Mahfouz</motion.h1>
+            <motion.p className="text-center xl:text-start max-w-[500px] mb-8 text-white/80 "
+              initial={{ translateX: -50, opacity: 0 }}
+              animate={{
+                opacity: 1,
+                translateX: 0,
+                transition: { delay: 1.1, duration: 0.7, ease: "easeInOut" }
+              }}>I excel at crafting digital experiences and I am proficient in various programming languages and technologies.</motion.p>
             {/* button and social */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button variant="outline" className="uppercase flex items-center gap-2 ">
-                <a href="/Muhammad Mahfouz Moawad Hadib CV.pdf" download="Muhammad Mahfouz CV" target="_blank">Download CV </a>
-                <FiDownload className="text-xl" />
-              </Button>
+              <motion.div
+                initial={{ translateX: -50, opacity: 0 }}
+                animate={{
+                  opacity: 1,
+                  translateX: 0,
+                  transition: { delay: 1.2, duration: 0.7, ease: "easeInOut" }
+                }}>
+                <Button variant="outline" className="uppercase flex items-center gap-2 ">
+                  <a href="/Muhammad Mahfouz Moawad Hadib CV.pdf" download="Muhammad Mahfouz CV" target="_blank">Download CV </a>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </motion.div>
+
               <div className="mb-8 xl:mb-0">
                 <Social containerStyle="flex gap-4" iconStyle="text-2xl hover:text-accent transition-colors" />
               </div>
+
             </div>
           </div>
           {/* photo */}
--- components/Social.jsx
+++ components/Social.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link"
 import { FaGithub, FaLinkedinIn, FaWhatsapp, FaFacebookF, FaCodepen } from "react-icons/fa"
+import { motion } from "framer-motion"
 
 const social = [
-    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/mohamed-mahfouz-moawad?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
-    { icon: <FaGithub />, path: "https://github.com/MohamedMahfouz2022" },
-    { icon: <FaWhatsapp />, path: "https://wa.me/qr/SWV2KAXH6DCPL1" },
-    { icon: <FaFacebookF />, path: "https://www.facebook.com/mohamed.mahfouz.35175?mibextid=ZbWKwL" },
-    { icon: <FaCodepen />, path: "https://codepen.io/mohamedmahfouz2022/pens/public" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/mohamed-mahfouz-moawad?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", delayIcon: 1.1 },
+    { icon: <FaGithub />, path: "https://github.com/MohamedMahfouz2022", delayIcon: 1.2 },
+    { icon: <FaWhatsapp />, path: "https://wa.me/qr/SWV2KAXH6DCPL1", delayIcon: 1.3 },
+    { icon: <FaFacebookF />, path: "https://www.facebook.com/mohamed.mahfouz.35175?mibextid=ZbWKwL", delayIcon: 1.4 },
+    { icon: <FaCodepen />, path: "https://codepen.io/mohamedmahfouz2022/pens/public", delayIcon: 1.5 },
 ]
 
 const Social = ({ containerStyle, iconStyle }) => {
@@ -14,9 +15,18 @@ const Social = ({ containerStyle, iconStyle }) => {
         <div className={containerStyle}>
             {social.map((item, index) => {
                 return (
-                    <Link key={index} href={item.path} target="_blank" className={iconStyle} >
-                        {item.icon}
-                    </Link>
+                    <motion.span key={index}
+                        initial={{ translateY: 50, opacity: 0 }}
+                        animate={{
+                            opacity: 1,
+                            translateY: 0,
+                            transition: { delay: item.delayIcon, duration: .8, ease: "easeInOut" }
+                        }}>
+                        <Link href={item.path} target="_blank" className={iconStyle} >
+                            {item.icon}
+                        </Link>
+                    </motion.span>
+
                 )
             })}
         </div>
