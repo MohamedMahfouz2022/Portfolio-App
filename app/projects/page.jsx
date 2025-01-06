137a6ce2828180b49891b340780f868a7d1254c1
Mohamed_Mahfouz <wrzdwrzd@gmail.com>
2025-01-06 16:15:33 +0200

    modified Repos components

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,140 +1,122 @@
-"use client"
-import { FaHtml5, FaCss3, FaJs, FaNodeJs } from "react-icons/fa"
-import Link from "next/link"
-import { motion } from "framer-motion"
-
+"use client";
+// import { FaHtml5, FaCss3, FaJs, FaNodeJs } from "react-icons/fa";
+// import Link from "next/link";
+import { motion } from "framer-motion";
+import Repos from "@/components/Repos";
 // list of project
-const listOfProject = [
-  {
-    num: "01",
-    category: "Frontend",
-    title: "blogr-landing-page |",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
-    stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }],
+// const listOfProject = [
+//   {
+//     num: "01",
+//     category: "Frontend",
+//     title: "blogr-landing-page |",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
+//     stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }],
 
-    live: "live demo",
-    urlLive: "https://mohamedmahfouz2022.github.io/blogr-landing-page-main/",
-    github: "Github link",
-    urlGithub: "https://github.com/MohamedMahfouz2022/blogr-landing-page-main.git",
-    delayCard: 0.85
-  },
-  {
-    num: "02",
-    category: "Frontend",
-    title: "To-do-list |",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
-    stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }],
+//     live: "live demo",
+//     urlLive: "https://mohamedmahfouz2022.github.io/blogr-landing-page-main/",
+//     github: "Github link",
+//     urlGithub:
+//       "https://github.com/MohamedMahfouz2022/blogr-landing-page-main.git",
+//     delayCard: 0.85,
+//   },
+//   {
+//     num: "02",
+//     category: "Frontend",
+//     title: "To-do-list |",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
+//     stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }],
 
-    live: "live demo",
-    urlLive: "https://mohamedmahfouz2022.github.io/To-Do-List/",
-    github: "Github link",
-    urlGithub: "https://github.com/MohamedMahfouz2022/To-Do-List",
-    delayCard: 0.9
-  },
-  {
-    num: "03",
-    category: "Frontend",
-    title: "Zenbrew Coffee Site |",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
-    stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }, { icon: <FaNodeJs /> }],
+//     live: "live demo",
+//     urlLive: "https://mohamedmahfouz2022.github.io/To-Do-List/",
+//     github: "Github link",
+//     urlGithub: "https://github.com/MohamedMahfouz2022/To-Do-List",
+//     delayCard: 0.9,
+//   },
+//   {
+//     num: "03",
+//     category: "Frontend",
+//     title: "Zenbrew Coffee Site |",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
+//     stack: [
+//       { icon: <FaHtml5 /> },
+//       { icon: <FaCss3 /> },
+//       { icon: <FaJs /> },
+//       { icon: <FaNodeJs /> },
+//     ],
 
-    live: "live demo",
-    urlLive: "https://zenbrew-coffee-site-git-main-muhammad-mahfouz-s-projects.vercel.app/",
-    github: "Github link",
-    urlGithub: "https://github.com/MohamedMahfouz2022/Zenbrew-Coffee-Site",
-    delayCard: .95
-  },
-  {
-    num: "04",
-    category: "Frontend",
-    title: "Landing Page project |",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
-    stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }],
+//     live: "live demo",
+//     urlLive:
+//       "https://zenbrew-coffee-site-git-main-muhammad-mahfouz-s-projects.vercel.app/",
+//     github: "Github link",
+//     urlGithub: "https://github.com/MohamedMahfouz2022/Zenbrew-Coffee-Site",
+//     delayCard: 0.95,
+//   },
+//   {
+//     num: "04",
+//     category: "Frontend",
+//     title: "Landing Page project |",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
+//     stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }],
 
-    live: "live demo",
-    urlLive: "https://mohamedmahfouz2022.github.io/cd0428-landing-page-main/",
-    github: "Github link",
-    urlGithub: "https://github.com/MohamedMahfouz2022/cd0428-landing-page-main ",
-    delayCard: 1
-  },
-  {
-    num: "05",
-    category: "Frontend",
-    title: "Safari |",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
-    stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }],
+//     live: "live demo",
+//     urlLive: "https://mohamedmahfouz2022.github.io/cd0428-landing-page-main/",
+//     github: "Github link",
+//     urlGithub:
+//       "https://github.com/MohamedMahfouz2022/cd0428-landing-page-main ",
+//     delayCard: 1,
+//   },
+//   {
+//     num: "05",
+//     category: "Frontend",
+//     title: "Safari |",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
+//     stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }],
 
-    live: "live demo",
-    urlLive: "https://mohamedmahfouz2022.github.io/safari/",
-    github: "Github link",
-    urlGithub: "https://github.com/MohamedMahfouz2022/safari",
-    delayCard: 1.1
-  },
-  {
-    num: "06",
-    category: "Frontend",
-    title: "Restaurant-app |",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
-    stack: [{ icon: <FaHtml5 /> }, { icon: <FaCss3 /> }, { icon: <FaJs /> }, { icon: <FaNodeJs /> }],
+//     live: "live demo",
+//     urlLive: "https://mohamedmahfouz2022.github.io/safari/",
+//     github: "Github link",
+//     urlGithub: "https://github.com/MohamedMahfouz2022/safari",
+//     delayCard: 1.1,
+//   },
+//   {
+//     num: "06",
+//     category: "Frontend",
+//     title: "Restaurant-app |",
+//     description:
+//       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, ipsa!",
+//     stack: [
+//       { icon: <FaHtml5 /> },
+//       { icon: <FaCss3 /> },
+//       { icon: <FaJs /> },
+//       { icon: <FaNodeJs /> },
+//     ],
 
-    live: "live demo",
-    urlLive: "https://restaurant-app-five-omega.vercel.app//",
-    github: "Github link",
-    urlGithub: "https://github.com/MohamedMahfouz2022/restaurant-app",
-    delayCard: 1.2
-  },
-]
+//     live: "live demo",
+//     urlLive: "https://restaurant-app-five-omega.vercel.app//",
+//     github: "Github link",
+//     urlGithub: "https://github.com/MohamedMahfouz2022/restaurant-app",
+//     delayCard: 1.2,
+//   },
+// ];
 
 const Projects = () => {
-  return <motion.section className="container mx-auto"
-    initial={{ opacity: 0 }}
-    animate={{
-      opacity: 1,
-      transition: { delay: .7, duration: 0.4, ease: "easeInOut" }
-    }}>
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-      {listOfProject.map((project, index) => {
-        return (
-          <motion.div key={index} className="mx-auto my-5 max-w-xs rounded-xl text-white shadow 
-						bg-[#232329] p-3 hover:-translate-y-3 transition-all duration-500 ease-in-out"
-            initial={{ opacity: 0, rotate: -10 }}
-            animate={{
-              opacity: 1,
-              rotate: 0,
-              transition: { delay: project.delayCard, duration: .4, ease: "easeInOut" }
-            }}>
-            {/* titel card */}
-            <div className="flex flex-row justify-between items-center">
-              <div className="flex flex-col gap-3">
-                <div className="flex justify-between w-fit rounded-md bg-blue-100 px-3 py-1 text-sm text-primary text-center font-bold">
-                  {project.category}
-                </div>
-                <p className="text-xl">{project.title}</p>
-                <span className="flex flex-row gap-3">{project.stack.map((icons, index) => { return (<span key={index}>{icons.icon}</span>) })}</span>
-              </div>
-              {/* num of cards */}
-              <div className="px-3 text-5xl leading-none font-extrabold text-opacity-50 rotate-12 ">{project.num}
-              </div>
-            </div>
+  return (
+    <motion.section
+      className="container mx-auto"
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 0.7, duration: 0.4, ease: "easeInOut" },
+      }}
+    >
+      <Repos />
+    </motion.section>
+  );
+};
 
-            {/* project description */}
-            <p className="my-3 text-gray-400">{project.description}</p>
-
-            {/* url link */}
-            <div className="flex flex-row gap-3">
-              <Link href={project.urlLive} target="_blank" className="">
-                <span className="text-accent hover:text-accent-hover"> {project.live} </span>
-              </Link>
-              <Link href={project.urlGithub} target="_blank" className="">
-                <span className="text-accent hover:text-accent-hover"> {project.github} </span>
-              </Link>
-            </div>
-          </motion.div>
-        )
-      })}
-    </div>
-  </motion.section>
-
-}
-
-export default Projects
+export default Projects;
---
+++ components/Repos.jsx
@@ -0,0 +1,102 @@
+"use client";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import { FaHtml5, FaCss3, FaJs } from "react-icons/fa";
+
+const Repos = () => {
+  const [repos, setRepos] = useState([]);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const response = await fetch(
+          // "https://api.github.com/users/MohamedMahfouz2022/repos"
+          "http://localhost:4000/repos"
+        );
+        const data = await response.json();
+        setRepos(data);
+      } catch (error) {
+        console.error("Error fetching repos:", error);
+      }
+    };
+
+    fetchRepos();
+  }, []);
+
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold text-center mb-6">
+        My GitHub Projects
+      </h1>
+      <div
+        className="h-[70vh] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6  overflow-scroll scroll-smooth "
+        style={{ scrollbarWidth: "none" }}
+      >
+        {repos.map((repo, index) => (
+          <motion.div
+            key={index}
+            className=" mx-auto my-5 max-w-xs min-w-80 rounded-xl text-white shadow 
+						bg-[#232329] p-3 hover:-translate-y-3 transition-all duration-500 ease-in-out hover:scale-50 "
+            initial={{ opacity: 0, rotate: -10 }}
+            animate={{
+              opacity: 1,
+              rotate: 0,
+              transition: {
+                delay: 0.85,
+                duration: 0.4,
+                ease: "easeInOut",
+              },
+            }}
+          >
+            {/* -------------- */}
+            {/* titel and num card */}
+            <div className="flex flex-row justify-between items-center">
+              {/* titel*/}
+              <div className="flex flex-col gap-3">
+                <div className="flex justify-between w-fit rounded-md bg-blue-100 px-3 py-1 text-sm text-primary text-center font-bold">
+                  Frontend
+                </div>
+                <p className="text-xl">{repo.name} |</p>
+                <span className="flex flex-row gap-3 text-accent">
+                  <FaHtml5 /> | <FaCss3 /> | <FaJs />
+                </span>
+              </div>
+
+              {/* num of cards */}
+              <div className="px-3 text-5xl leading-none font-extrabold text-opacity-50 rotate-12 ">
+                {index + 1}
+              </div>
+            </div>
+            {/* -------------- */}
+
+            {/* project description */}
+            <p className="my-3 text-gray-400">
+              {repo.description || repo.created_at}
+            </p>
+
+            {/* url link */}
+            <div className="flex flex-row gap-3">
+              {repo.homepage && (
+                <Link href={repo.homepage} target="_blank" className="">
+                  <span className="text-accent hover:text-accent-hover">
+                    live Demo
+                  </span>
+                </Link>
+              )}
+              {repo.html_url && (
+                <Link href={repo.html_url} target="_blank" className="">
+                  <span className="text-accent hover:text-accent-hover">
+                    View on GitHub
+                  </span>
+                </Link>
+              )}
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Repos;
